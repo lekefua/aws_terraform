f8beed524aaaa22a18e1bd17a906a0f887b050c5
lekefua <145169940+lekefua@users.noreply.github.com>
2024-01-23 01:56:13 +0100

    Update s3.tf

--- storage/s3.tf
+++ storage/s3.tf
@@ -1,1 +1,13 @@
+provider "aws" {
+  region = "var.aws_region"
+access_key=var.AWS_ACCESS_KEY
+secret_key=var.AWS_SECRET_ACCESS_KEY
+}
+
+resource "aws_s3_bucket" "bucket1" {
+count=2
+tags={name="test-bucket-${count.index}"
+}
+}
+
 
